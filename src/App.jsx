4e5938422b94b352a01de293f5f028a9e1630ace
Markopamichh <markopamich@gmail.com>
2025-10-14 20:40:10 -0300

    2do commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Fragment } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [counter, setCounter] = useState(0)
+  const [nombre, setNombre] = useState('marko')
+  console.log('counter', counter)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>contador: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>incrementar</button>
+      <button onClick={() => setCounter(counter - 1)}>decrementar</button>
+      <button onClick={() => setCounter(0)}>resetear</button>
+      <p>nombre: {nombre}</p>
+      <button onClick={() => setNombre('kito')}>cambiar nombre</button>
+
     </>
   )
 }
